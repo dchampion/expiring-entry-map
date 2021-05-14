db22c7a975b8c6c73f498542d920b9105971eb2d
dchampion <divaconhamdip@gmail.com>
2021-05-14 10:17:29 -0600

    Initial commit.

---
+++ src/main/java/com/dchampion/ExpiringEntryMap.java
@@ -0,0 +1,341 @@
+package com.dchampion;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A map (i.e. "dictionary") with expiring entries. Users of this class should supply their own
+ * concrete {@link Map} implementation to this class's constructor, and an optional parameter
+ * specifying the time after which the map's entries should expire. If the optional "lifetime"
+ * parameter is omitted, the map's entries will be set to expire after a default lifetime of 60
+ * minutes.
+ * <p>
+ * Users can construct an instance of this class via its static {@link Builder} methods, and use it
+ * as they would any standard {@link Map} implementation; e.g.,
+ * <pre>
+ * // Construct a java.util.HashMap instance and populate it with entries.
+ * Map&lt;Integer,String> myHashMap = new HashMap<>();
+ * myHashMap.put(1, "one");
+ * myHashMap.put(2, "two");
+ * 
+ * // Construct an ExpiringEntryMap backed by the HashMap, with entries that will
+ * // be removed (i.e., expire) 100 milliseconds after they have been put into the
+ * // map. Note the lifetime() method can be omitted, in which case the map's entries
+ * // will be set to expire after a default lifetime of 60 minutes.
+ * Map&lt;Integer,String> myExpiringEntryMap = 
+ *      ExpiringEntryMap.Builder.map(myHashMap).lifetime(TimeUnit.MILLISECONDS, 100).build();
+ * 
+ * // Use the ExpiringEntryMap as you would use any other HashMap.
+ * if (myExpiringEntryMap.get(1).equals("one")) {
+ *      myExpiringEntryMap.put(1, "ONE");
+ *      assert myExpiringEntryMap.get(1).equals("ONE");
+ * }
+ * 
+ * // Sleep for 100 milliseconds; a call to isEmpty() should return true, as the previous entries
+ * // should have by now expired.
+ * Thread.sleep(100);
+ * assert myExpiringEntryMap.isEmpty();
+ * </pre>
+ * Users can expect instances of this class to behave in a manner consistent with the {@link Map}
+ * implementation supplied to its constructor; that is, instances of {@link HashMap},
+ * {@link java.util.LinkedHashMap}, {@link java.util.TreeMap}, or any concrete {@link Map}
+ * implementation for that matter, will behave as expected when wrapped by an instance of this class.
+ */
+public final class ExpiringEntryMap<K,V> implements Map<K,V> {
+    
+    @SuppressWarnings("rawtypes")
+    private Map wrapper = null;
+    
+    /**
+     * Lifetime of map values (in milliseconds).
+     */
+    long lifetime;
+
+    /**
+     * A wrapper that timestamps map values.
+     *
+     * @param <V> The map's value.
+     */
+    private static final class TimestampedValue<V> {
+        final V value;
+        final long timestamp;
+
+        TimestampedValue(V value, long timestamp) {
+            this.value = value;
+            this.timestamp = timestamp;
+        }
+
+        V getValue() {
+            return value;
+        }
+
+        long getTimestamp() {
+            return timestamp;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == this) {
+                return true;
+            }
+            if (!(o instanceof TimestampedValue)) {
+                return false;
+            }
+            TimestampedValue<?> other = (TimestampedValue<?>) o;
+            if (other.getValue() != null && getValue() == null) {
+                return false;
+            }
+            if (other.getValue() == null && getValue() != null) {
+                return false;
+            }
+            if (other.getValue() == null && getValue() == null) {
+                return true;
+            }
+            return other.getValue().equals(getValue());
+        }
+
+        @Override
+        public int hashCode() {
+            return getValue() != null ? getValue().hashCode() : 31;
+        }
+    }
+
+    /**
+     * Use this class to construct instances of {@link ExpiringEntryMap}.
+     */
+    public static final class Builder<K,V> {
+        private Map<K,V> map;
+        private long lifetimeInMillis = 60 * 60 * 1000;
+
+        /**
+         * Returns an instance of this {@link ExpiringEntryMap}'s builder.
+         * 
+         * @param <K> The data type of the map's keys
+         * @param <V> The data type of the map's values
+         * @param map The map, which can be empty or prepopulated with entries.
+         * 
+         * @return An instance of this builder.
+         * 
+         * @throws IllegalArgumentException if the supplied map is null.
+         */
+        public static <K,V> Builder<K,V> map(Map<K,V> map) {
+            if (map == null) {
+                throw new IllegalArgumentException("map cannot be null");
+            }
+            Builder<K,V> builder = new Builder<K,V>(map);
+            return builder;
+        }
+
+        /**
+         * Specifies the lifetime of this {@link ExpiringEntryMap}'s entries. This method
+         * is optional and, if omitted, the map's entries will be set to expire after a default
+         * lifetime of 60 minutes.
+         * 
+         * @param unit The {@link TimeUnit}
+         * @param lifetime The number of {@link TimeUnit}s after which this map's entries will expire.
+         * 
+         * @return An instance of this builder.
+         * 
+         * @throws IllegalArgumentException if the supplied unit is either null or finer than
+         * {@link TimeUnit#MILLISECONDS} resolution, or if lifetime is less than 1.
+         */
+        public Builder<K,V> lifetime(TimeUnit unit, long lifetime) {
+            if (unit == null) {
+                throw new IllegalArgumentException("unit must be specified");
+            }
+            if (unit == TimeUnit.MICROSECONDS || unit == TimeUnit.NANOSECONDS) {
+                throw new IllegalArgumentException("unit must be of resolution " + TimeUnit.MILLISECONDS + " or greater");
+            }
+            if (lifetime < 1) {
+                throw new IllegalArgumentException("lifetime must be one " + unit + " or greater");
+            }
+            convertLifetimeToMillis(unit, lifetime);
+            return this;
+        }
+
+        /**
+         * The terminal operation of this builder, which returns a new {@link ExpriringEntryMap} instance.
+         * 
+         * @return An {@link ExpiringEntryMap} instance.
+         */
+        public ExpiringEntryMap<K,V> build() {
+            return new ExpiringEntryMap<>(this.map, this.lifetimeInMillis);
+        }
+
+        private Builder(Map<K,V> map) {
+            this.map = map;
+        }
+
+        private void convertLifetimeToMillis(TimeUnit unit, long lifetimeInTimeUnit) {
+            switch(unit) {
+                case DAYS:
+                    lifetimeInMillis = lifetimeInTimeUnit * 24 * 60 * 60 * 1_000;
+                    break;
+                case HOURS:
+                    lifetimeInMillis = lifetimeInTimeUnit * 60 * 60 * 1_000;
+                    break;
+                case MINUTES:
+                    lifetimeInMillis = lifetimeInTimeUnit * 60 * 1_000;
+                    break;
+                case SECONDS:
+                    lifetimeInMillis = lifetimeInTimeUnit * 1_000;
+                    break;
+                case MILLISECONDS:
+                    lifetimeInMillis = lifetimeInTimeUnit;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private ExpiringEntryMap(Map<K,V> map, long lifetime) {
+        assert map != null;
+        assert lifetime > 0;
+        long timestamp = now();
+        try {
+            wrapper = map.getClass().getDeclaredConstructor((Class<?>[])null).newInstance((Object[])null);
+            for(Entry<K,V> entry : map.entrySet()) {
+                wrapper.put(entry.getKey(), new TimestampedValue<V>(entry.getValue(), timestamp));
+            }
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+        this.lifetime = lifetime;
+    }
+
+    @Override
+    public int size() {
+        flush();
+        return wrapper.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        flush();
+        return wrapper.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        flush();
+        return wrapper.containsKey(key);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean containsValue(Object value) {
+        flush();
+        for (Entry<K,TimestampedValue<V>> entry : (Set<Entry<K,TimestampedValue<V>>>) wrapper.entrySet()) {
+            V val = entry.getValue().getValue();
+            if (val != null && val.equals(value)) {
+                return true;
+            }
+            if (val == null && value == null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        flush();
+        @SuppressWarnings("unchecked")
+        TimestampedValue<V> ev = (TimestampedValue<V>) wrapper.get(key);
+        return ev != null ? ev.getValue() : null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        flush();
+        @SuppressWarnings("unchecked")
+        TimestampedValue<V> ev = (TimestampedValue<V>) wrapper.put(key, new TimestampedValue<V>(value, now()));
+        return ev != null ? ev.getValue() : null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        flush();
+        @SuppressWarnings("unchecked")
+        TimestampedValue<V> ev = (TimestampedValue<V>) wrapper.remove(key);
+        return ev != null ? ev.getValue() : null;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void putAll(Map<? extends K,? extends V> m) {
+        flush();
+        long now = now();
+        for (Entry<? extends K,? extends V> entry : m.entrySet()) {
+            wrapper.put((K)entry.getKey(), (TimestampedValue<V>) new TimestampedValue<V>(entry.getValue(), now));
+        }
+    }
+
+    @Override
+    public synchronized void clear() {
+        wrapper.clear();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Set<K> keySet() {
+        flush();
+        return wrapper.keySet();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Collection<V> values() {
+        flush();
+        List<V> values = new ArrayList<V>();
+        for (TimestampedValue<V> ev : (Collection<TimestampedValue<V>>) wrapper.values()) {
+            values.add(ev.getValue());
+        }
+        return values;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Set<Entry<K,V>> entrySet() {
+        flush();
+        // Implementation doesn't really matter here.
+        Map<K,V> map = new HashMap<K,V>();
+        for (Entry<K,TimestampedValue<V>> entry : (Set<Entry<K,TimestampedValue<V>>>) wrapper.entrySet()) {
+            map.put(entry.getKey(), entry.getValue().getValue());
+        }
+        return map.entrySet();
+    }
+
+    @SuppressWarnings("unchecked")
+    private synchronized void flush() {
+        long now = now();
+        Set<Entry<K,TimestampedValue<V>>> entrySet = Set.copyOf((Set<Entry<K,TimestampedValue<V>>>)wrapper.entrySet());
+        for (Entry<K,TimestampedValue<V>> entry : entrySet) {
+            if (now - entry.getValue().getTimestamp() > lifetime) {
+                wrapper.remove(entry.getKey());
+            }
+        }
+    }
+
+    private long now() {
+        return System.currentTimeMillis();
+    }
+}
---
+++ src/test/java/com/dchampion/ExpiringEntryMapTest.java
@@ -0,0 +1,338 @@
+package com.dchampion;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExpiringEntryMapTest {
+
+    ArrayList<Map<Integer,String>> maps = new ArrayList<>();
+
+    @Before
+    public void setup() {
+        createMaps(TimeUnit.SECONDS, 10);
+    }
+
+    void createMaps(TimeUnit unit, long lifetime) {
+        // HashMap
+        maps.add(ExpiringEntryMap.Builder.map(new HashMap<Integer,String>()).lifetime(unit, lifetime).build());
+        // TreeMap
+        maps.add(ExpiringEntryMap.Builder.map(new TreeMap<Integer,String>()).lifetime(unit, lifetime).build());
+        // Hashtable
+        maps.add(ExpiringEntryMap.Builder.map(new Hashtable<Integer,String>()).lifetime(unit, lifetime).build());
+        // LinkedHashMap
+        maps.add(ExpiringEntryMap.Builder.map(new LinkedHashMap<Integer,String>()).lifetime(unit, lifetime).build());
+    }
+
+    @After
+    public void tearDown() {
+        clearMaps();
+    }
+
+    void clearMaps() {
+        maps.clear();
+    }
+
+    // Begin Builder functionality tests.
+    @Test
+    public void testBuildMap() {
+        Map<String,Integer> map = ExpiringEntryMap.Builder.map(new HashMap<String,Integer>()).build();
+        map.put("one", 1);
+        assertEquals(map.get("one"), Integer.valueOf(1));
+
+        map = ExpiringEntryMap.Builder.map(new HashMap<String,Integer>()).lifetime(TimeUnit.HOURS, 2).build();
+        map.put("one", 1);
+        assertEquals(map.get("one"), Integer.valueOf(1));
+    }
+
+    @Test
+    public void testTimeUnitConversions() {
+        ExpiringEntryMap<String,Integer> map = 
+            ExpiringEntryMap.Builder.map(new HashMap<String,Integer>()).build();
+        assertEquals(map.lifetime, 60 * 60 * 1000);
+
+        map = ExpiringEntryMap.Builder.map(new HashMap<String,Integer>()).lifetime(TimeUnit.DAYS, 1).build();
+        assertEquals(map.lifetime, 24 * 60 * 60 * 1000);
+
+        map = ExpiringEntryMap.Builder.map(new HashMap<String,Integer>()).lifetime(TimeUnit.HOURS, 1).build();
+        assertEquals(map.lifetime, 60 * 60 * 1000);
+
+        map = ExpiringEntryMap.Builder.map(new HashMap<String,Integer>()).lifetime(TimeUnit.MINUTES, 1).build();
+        assertEquals(map.lifetime, 60 * 1000);
+
+        map = ExpiringEntryMap.Builder.map(new HashMap<String,Integer>()).lifetime(TimeUnit.SECONDS, 1).build();
+        assertEquals(map.lifetime, 1000);
+
+        map = ExpiringEntryMap.Builder.map(new HashMap<String,Integer>()).lifetime(TimeUnit.MILLISECONDS, 1).build();
+        assertEquals(map.lifetime, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullMap() {
+        ExpiringEntryMap.Builder.map(null);
+    }
+
+    @Test
+    public void testLifetimeExceptions() {
+        try {
+            ExpiringEntryMap.Builder.map(new HashMap<String,String>()).lifetime(TimeUnit.MICROSECONDS, 1);
+            fail("Should have thrown IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            ExpiringEntryMap.Builder.map(new HashMap<String,String>()).lifetime(TimeUnit.NANOSECONDS, 1);
+            fail("Should have thrown IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            ExpiringEntryMap.Builder.map(new HashMap<String,String>()).lifetime(TimeUnit.MILLISECONDS, -1);
+            fail("Should have thrown IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+    // End Builder functionality tests.
+
+    // Begin standard map functionality tests.
+    void populate(Map<Integer,String> map) {
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+        map.put(4, "four");
+    }
+
+    @Test
+    public void testClear() {
+        maps.forEach(map -> {
+            populate(map);
+            map.clear();
+            assertTrue(map.isEmpty());
+        });
+    }
+
+    @Test
+    public void testCompute() {
+        maps.forEach(map -> {
+            populate(map);
+            assertEquals(map.compute(3, (k, v) -> v.concat("blindmice")), "threeblindmice");
+            assertEquals(map.get(3), "threeblindmice");
+        });
+    }
+
+    @Test
+    public void testComputeIfAbsent() {
+        maps.forEach(map -> {
+            populate(map);
+            assertEquals(map.computeIfAbsent(3, k -> "three"), "three");
+            assertEquals(map.get(3), "three");
+            assertNull(map.get(5));
+            assertEquals(map.computeIfAbsent(5, k -> "five"), "five");
+            assertEquals(map.get(5), "five");
+        });
+    }
+
+    @Test
+    public void testComputeIfPresent() {
+        maps.forEach(map -> {
+            populate(map);
+            assertEquals(map.computeIfPresent(3, (k, v) -> "THREE"), "THREE");
+            assertEquals(map.get(3), "THREE");
+            assertNull(map.get(5));
+            assertNull(map.computeIfPresent(5, (k, v) -> "five"));
+            assertNull(map.get(5));
+        });
+    }
+
+    @Test
+    public void testContainsKey() {
+        maps.forEach(map -> {
+            populate(map);
+            assertTrue(map.containsKey(4));
+            assertFalse(map.containsKey(5));
+        });
+    }
+
+    @Test
+    public void testContainsValue() {
+        maps.forEach(map -> {
+            populate(map);
+            assertTrue(map.containsValue("two"));
+            assertFalse(map.containsValue("five"));
+        });
+    }
+
+    @Test
+    public void testEntrySet() {
+        maps.forEach(map -> {
+            populate(map);
+            Set<Entry<Integer,String>> entrySet = map.entrySet();
+            assertEquals(entrySet.size(), 4);
+            entrySet.forEach(entry -> {
+                assertTrue(entry.getKey() instanceof Integer);
+                assertTrue(entry.getValue() instanceof String);
+            });
+        });
+    }
+
+    @Test
+    public void testGet() {
+        maps.forEach(map -> {
+            populate(map);
+            assertEquals("one", map.get(1));
+            assertEquals("two", map.get(2));
+            assertEquals("three", map.get(3));
+            assertEquals("four", map.get(4));
+            assertEquals(null, map.get(5));
+        });
+    }
+
+    @Test
+    public void testGetOrDefault() {
+        maps.forEach(map -> {
+            populate(map);
+            assertEquals("one", map.getOrDefault(1, "one"));
+            assertEquals("two", map.getOrDefault(2, "two"));
+            assertEquals("three", map.getOrDefault(3, "three"));
+            assertEquals("four", map.getOrDefault(4, "four"));
+            assertEquals("five", map.getOrDefault(5, "five"));
+        });
+    }
+
+    @Test
+    public void testIsEmpty() {
+        maps.forEach(map -> {
+            assertTrue(map.isEmpty());
+            populate(map);
+            assertFalse(map.isEmpty());
+        });
+    }
+
+    @Test
+    public void testKeySet() {
+        maps.forEach(map -> {
+            populate(map);
+            Set<Integer> keySet = map.keySet();
+            assertEquals(4, keySet.size());
+            assertTrue(keySet.containsAll(Arrays.asList(1, 2, 3, 4)));
+        });
+    }
+
+    @Test
+    public void testMerge() {
+        maps.forEach(map -> {
+            populate(map);
+            assertEquals(map.merge(1, "ofakind", String::concat), "oneofakind");
+            assertEquals(map.get(1), "oneofakind");
+        });
+    }
+
+    @Test
+    public void testPut() {
+        maps.forEach(map -> {
+            assertTrue(map.isEmpty());
+            assertNull(map.put(1, "one"));
+            assertEquals(map.get(1), "one");
+            assertEquals(map.put(1, "ONE"), "one");
+            assertEquals(map.get(1), "ONE");
+        });
+    }
+
+    @Test
+    public void putAll() {
+        maps.forEach(map -> {
+            assertTrue(map.isEmpty());
+            Map<Integer,String> m = new HashMap<>();
+            m.put(1, "one");
+            m.put(2, "two");
+            map.putAll(m);
+            assertEquals(map.get(1), "one");
+            assertEquals(map.get(2), "two");
+        });
+    }
+
+    @Test
+    public void putIfAbsent() {
+        maps.forEach(map -> {
+            populate(map);
+            assertEquals(map.putIfAbsent(1, "one"), "one");
+            assertNull(map.putIfAbsent(5, "five"));
+            assertEquals(map.get(5), "five");
+        });
+    }
+
+    @Test
+    public void testRemove() {
+        maps.forEach(map -> {
+            populate(map);
+            assertFalse(map.isEmpty());
+            assertEquals(map.remove(1), "one");
+            assertEquals(map.remove(2), "two");
+            assertEquals(map.remove(3), "three");
+            assertEquals(map.remove(4), "four");
+            assertNull(map.remove(4));
+        });
+    }
+
+    @Test
+    public void testSize() {
+        maps.forEach(map -> {
+            populate(map);
+            assertEquals(map.size(), 4);
+            map.clear();
+            assertEquals(map.size(), 0);
+        });
+    }
+
+    @Test
+    public void testSerialize() {
+        // TODO: Implement me.
+    }
+    // End standard map functionality tests.
+
+    // Begin expiring map value functionality tests.
+    void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testEntriesExpire() {
+        clearMaps();
+        createMaps(TimeUnit.MILLISECONDS, 100);
+        maps.forEach(map -> {
+            populate(map);
+            assertEquals(map.size(), 4);
+            sleep(200);
+            assertEquals(map.size(), 0);
+
+            map.put(1, "ONE");
+            assertEquals(map.size(), 1);
+            sleep(200);
+            assertEquals(map.size(), 0);
+
+            map.put(2, "TWO");
+            sleep(200);
+            assertEquals(map.size(), 0);
+        });
+    }
+    // End expiring map value functionality tests.
+}
